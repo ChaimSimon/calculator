4d3fe9c50f268dd20afe976cb45395de93d41895
shalom lau <sholem.laufer@gmail.com>
2025-02-12 14:37:45 +0200

    punktion

--- calculator.py
+++ calculator.py
@@ -10,10 +10,22 @@ class calculator(ABC):
         pass
     
     def mul(a,b):
-        pass
+        x = int(input("enter a number"))
+        y = int(input("enter a number"))
+        c = 0
+        for i in range(y):
+            c += x
+        return c
         
     def div(a,b):
-        pass
+        x = int(input("enter a number"))
+        y = int(input("enter a number"))
+        c = x
+        d = 0
+        while c:
+            c -= y
+            d += 1
+        return d
         
     def pow(a,b):
         pass
