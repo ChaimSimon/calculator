82eecfdf94599dea68783f16fa64dbdceae5b7a0
ChaimSimon <chaimsimon2001@gmali.com>
2025-02-12 12:53:21 +0200

    class calculator

--- calculator.py
+++ calculator.py
@@ -1,1 +1,26 @@
-d 
+class calculator(ABC):
+
+
+    def add(a,b):
+        pass
+        
+    def sub(a,b):
+        pass
+    
+    def mul(a,b):
+        pass
+        
+    def div(a,b):
+        pass
+        
+    def pow(a,b):
+        pass
+        
+    def root(a,b):
+        pass
+        
+    
+        
+        
+          
+    
